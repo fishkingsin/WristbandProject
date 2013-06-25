162dd8685455948fd0c4ab9ebdae0368e3d078f6
James Kong <fishkingsin@gmail.com>
2013-06-25 17:38:16 +0800

    added tabed callback

--- Wristband/src/com/idthk/wristband/ui/SleepStatisticTabFragment.java
+++ Wristband/src/com/idthk/wristband/ui/SleepStatisticTabFragment.java
@@ -1,6 +1,7 @@
 package com.idthk.wristband.ui;
 
 import com.idthk.wristband.ui.R;
+import com.idthk.wristband.ui.ActivityStatisticTabFragment.OnFragmentTabbedListener;
 
 import android.app.ActionBar.LayoutParams;
 import android.app.Activity;
@@ -28,10 +29,20 @@ public class SleepStatisticTabFragment extends Fragment implements OnTabChangeLi
 	private View mRoot;
 	private TabHost mTabHost;
 	private int mCurrentTab;
+	public interface OnFragmentTabbedListener {
+		public void onSleepStatisticTabbed(String s);
 
+	}
+	OnFragmentTabbedListener mCallback;
 	@Override
 	public void onAttach(Activity activity) {
 		super.onAttach(activity);
+		try {
+            mCallback = (OnFragmentTabbedListener) activity;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(activity.toString()
+                    + " must implement OnHeadlineSelectedListener");
+        }
 	}
 
 	@Override
@@ -60,7 +71,7 @@ public class SleepStatisticTabFragment extends Fragment implements OnTabChangeLi
 		mTabHost.addTab(newTab(TAB_WEEK, R.string.tab_week, R.id.tab_week1));
 		mTabHost.addTab(newTab(TAB_MONTH, R.string.tab_month, R.id.tab_month2));
 		mTabHost.addTab(newTab(TAB_YEAR, R.string.tab_year, R.id.tab_year3));
-		
+		mCallback.onSleepStatisticTabbed(TAB_WEEK);
 	}
 
 	private TabSpec newTab(String tag, int labelId, int tabContentId) {
@@ -98,7 +109,7 @@ public class SleepStatisticTabFragment extends Fragment implements OnTabChangeLi
 	@Override
 	public void onTabChanged(String tabId) {
 		Log.d(TAG, "onTabChanged(): tabId=" + tabId);
-
+		mCallback.onSleepStatisticTabbed(tabId);
 //		if (TAB_DAY.equals(tabId)) {
 //			updateTab(tabId, R.id.tab_day1);
 //			mCurrentTab = 0;
